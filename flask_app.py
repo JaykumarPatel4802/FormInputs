6e2f0a7e99a53242d62000ad7e8f544aee125866
Jaykumar Patel <jaykumar.patel4802@gmail.com>
2018-12-11 23:25:40 +0000

    Added animals array to webpage

--- flask_app.py
+++ flask_app.py
@@ -13,4 +13,23 @@ def process_inputs():
     select = request.form.get('input_select', '')
     freeform = request.form.get('input_freeform', '')
     return render_template("main_page.html", input_data=dropdown,
-                           output="You're a wizard %s." % name)
+                            output="You're a wizard %s." % name,
+                            animals=dedupe(animals))
+
+
+def dedupe(x): # (x is the list)
+    y = []
+    for animal in x:
+        if animal not in y:
+            y+=animal
+        # add it
+    return y
+
+    # make a new list called deduped_list
+    # search for a duplicate
+    # for each element in x
+    # add item if it is no on a new list
+
+animals = ['cats', 'cats', 'cats', 'dogs', 'pandas']
+print(dedupe(animals)) # prints ['cats', 'dogs', 'pandas']
+
